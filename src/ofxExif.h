2d500b22ae2b4888e4433d143248423fb8c46e45
Erik Overmeire <eovermeire@gmail.com>
2012-09-16 14:31:08 +0200

    init ofxExiv2
    for accessing EXIF,IPTC and XMP image metadata in openframeworks

---
+++ src/ofxExif.h
@@ -0,0 +1,18 @@
+#ifndef OFX_EXIF_H
+#define OFX_EXIF_H
+
+#include "exiv2.hpp"
+#include "ofMain.h"
+
+//class comes here ------>
+class ofxExif{
+
+    public:
+        ofxExif();
+        ~ofxExif();
+
+};
+
+//<------
+
+#endif
---
+++ src/ofxExiv2.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "ofxExif.h"
+//#include "ofxIptc.h"
+//#include "ofxXmp.h"
